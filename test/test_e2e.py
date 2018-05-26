558424a01b2ea9b5550d1a54dac14b07e19dde0a
damian <ferraradamian@gmail.com>
2018-05-26 17:05:37 -0300

    Verificar que se haya solucionado el bug no mostraba el nombre del producto en la tabla

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -1,39 +1,55 @@
 import unittest
 import os
+import time
+import threading
 
 from selenium import webdriver
-from flask_testing import LiveServerTestCase
 
 from app import create_app, db
 from app.models import Product, Order, OrderProduct
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Ordering(LiveServerTestCase):
-    def create_app(self):
-        app = create_app()
-        app.config.update(
+class Ordering(unittest.TestCase):
+    #     # Creamos la base de datos de test
+    def setUp(self):
+        self.app = create_app()
+        self.app.config.update(
             SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'test.db'),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             TESTING=True
         )
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
-        return app
-
-    # Creamos la base de datos de test
-    def setUp(self):
-        self.baseURL = self.get_server_url()
+        self.baseURL = 'http://localhost:5000'
 
         db.session.commit()
         db.drop_all()
         db.create_all()
 
+        # start the Flask server in a thread
+        threading.Thread(target=self.app.run).start()
+
+        # give the server a second to ensure it is up
+        time.sleep(1)
+
         self.driver = webdriver.Chrome()
 
-    def test_title(self):
+    # def test_title(self):
+    #     driver = self.driver
+    #     driver.get(self.baseURL)
+    #     add_product_button = driver.find_element_by_xpath('/html/body/main/div[1]/div/button')
+    #     add_product_button.click()
+    #     modal = driver.find_element_by_id('modal')
+    #     assert modal.is_displayed(), "El modal no esta visible"
+
+    def test_nomb_produc(self):
         driver = self.driver
         driver.get(self.baseURL)
-        self.assertIn("Ordering", driver.title)
+        #nom_produc = driver.find_element_by_xpath("//td[contains(text(),'Individual')]")
+        nom_produc = driver.find_element_by_xpath("// html // tbody / tr[1] / td[2]")
+        assert nom_produc.text != "", "El nombre del producto no existe"
 
     def tearDown(self):
         db.session.remove()
@@ -42,4 +58,3 @@ class Ordering(LiveServerTestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
